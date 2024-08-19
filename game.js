6a318149d615d7cf966fca118a087e195565f076
Vale-barberis <valentinbarberis225@gmail.com>
2024-08-19 19:57:50 -0300

    Subiendo configuracion inicial del juego.

--- game.js
+++ game.js
@@ -1,1 +1,26 @@
-console.log("Hello World");
+/* global Phaser */
+
+const config = {
+    type: Phaser.AUTO,
+    width: 256,
+    height: 244,
+    backgroundColor: "#fff",
+    parent: "game",
+    scene: {
+        preload, // se ejecuta para precargar recursos
+        create, // se ejecuta cuando el juego comienza
+        update, // se ejecuta en cada frame
+    }
+}
+
+new Phaser.Game(config)
+
+function preload () {
+    console.log('preload')
+}
+
+function create () {
+    console.log('create')
+}
+
+function update () {}
