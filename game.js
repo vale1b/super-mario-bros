3bac86129ba994a96c132e134770e3ec2355dbb2
Vale-barberis <valentinbarberis225@gmail.com>
2024-08-20 15:16:52 -0300

    Mostrando assets y empezando con el desarrollo del videojuego.

--- game.js
+++ game.js
@@ -4,7 +4,7 @@ const config = {
     type: Phaser.AUTO,
     width: 256,
     height: 244,
-    backgroundColor: "#fff",
+    backgroundColor: "#049cd8",
     parent: "game",
     scene: {
         preload, // se ejecuta para precargar recursos
@@ -15,12 +15,35 @@ const config = {
 
 new Phaser.Game(config)
 
+// this --> game --> el juego que estamos construyendo
 function preload () {
-    console.log('preload')
+    this.load.image(
+        'cloud1',
+        'assets/scenery/overworld/cloud1.png'
+    )
+
+    this.load.image(
+        'floorbricks',
+        'assets/scenery/overworld/floorbricks.png'
+    )
+
+    this.load.spritesheet(
+        'mario',
+        'assets/entities/mario.png',
+        { frameWidth: 18, frameHeight: 16 }
+    )
 }
 
 function create () {
-    console.log('create')
+    this.add.image(100, 50, 'cloud1')
+        .setOrigin(0, 0)
+        .setScale(0.15)
+
+    this.add.tileSprite(0, config.height - 32, config.width, 32, 'floorbricks')
+        .setOrigin(0, 0)
+    
+    this.add.sprite(50, 210, 'mario')
+        .setOrigin(0, 1)
 }
 
 function update () {}
