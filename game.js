6bdcd11a6eaaa9fb4bfd2ce4aa8455e9957f615b
Vale-barberis <valentinbarberis225@gmail.com>
2024-08-22 15:33:54 -0300

    Arreglando bugs.

--- game.js
+++ game.js
@@ -24,54 +24,27 @@ const config = {
 
 new Phaser.Game(config)
 
-// this --> game --> el juego que estamos construyendo
 function preload () {
-    this.load.image(
-        'cloud1',
-        'assets/scenery/overworld/cloud1.png'
-    )
-
-    this.load.image(
-        'floorbricks',
-        'assets/scenery/overworld/floorbricks.png'
-    )
-
-    this.load.spritesheet(
-        'mario',
-        'assets/entities/mario.png',
-        { frameWidth: 18, frameHeight: 16 }
-    )
-
+    this.load.image('cloud1', 'assets/scenery/overworld/cloud1.png')
+    this.load.image('floorbricks', 'assets/scenery/overworld/floorbricks.png')
+    this.load.spritesheet('mario', 'assets/entities/mario.png', { frameWidth: 18, frameHeight: 16 })
     this.load.audio('gameover', 'assets/sound/music/gameover.mp3')
 }
 
 function create () {
-    this.add.image(100, 50, 'cloud1')
-        .setOrigin(0, 0)
-        .setScale(0.15)
+    this.add.image(100, 50, 'cloud1').setOrigin(0, 0).setScale(0.15)
 
     this.floor = this.physics.add.staticGroup()
+    this.floor.create(0, config.height - 16, 'floorbricks').setOrigin(0, 0.5).refreshBody()
+    this.floor.create(150, config.height - 16, 'floorbricks').setOrigin(0, 0.5).refreshBody()
 
-    this.floor 
-        .create(0, config.height - 16, 'floorbricks')
-        .setOrigin(0, 0.5)
-        .refreshBody()
-    
-    this.floor
-        .create(150, config.height - 16, 'floorbricks')
-        .setOrigin(0, 0.5)
-        .refreshBody()
-    
-    this.mario = this.physics.add.sprite(50, 210, 'mario')
-        .setOrigin(0, 1)
-        .setCollideWorldBounds(true)
-        .setGravityY(300)
+    this.mario = this.physics.add.sprite(50, 210, 'mario').setOrigin(0, 1).setCollideWorldBounds(true)
 
     this.physics.world.setBounds(0, 0, 2000, config.height)
     this.physics.add.collider(this.mario, this.floor)
 
-    this.camera.main.setBounds(0, 0, 2000, config.height)
-    this.camera.main.startFollow(this.mario)
+    this.cameras.main.setBounds(0, 0, 2000, config.height)
+    this.cameras.main.startFollow(this.mario)
 
     createAnimations(this)
 
@@ -82,14 +55,15 @@ function update () {
     if (this.mario.isDead) return
 
     if (this.keys.left.isDown) {
+        this.mario.setVelocityX(-160)
         this.mario.anims.play('mario-walk', true)
-        this.mario.x -= 2
         this.mario.flipX = true
     } else if (this.keys.right.isDown) {
+        this.mario.setVelocityX(160)
         this.mario.anims.play('mario-walk', true)
-        this.mario.x += 2
         this.mario.flipX = false
     } else {
+        this.mario.setVelocityX(0)
         this.mario.anims.play('mario-idle', true)
     }
 
